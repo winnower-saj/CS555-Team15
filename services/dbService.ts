3341ce30a5af09309bbbe3df8aa97b0badb46125
Dhanvin Patel <78669309+dhanvinpatel@users.noreply.github.com>
2024-10-29 13:44:42 -0400

    - Added dbService.ts

---
+++ services/dbService.ts
@@ -0,0 +1,76 @@
+import axios from 'axios';
+
+const Config = {
+    API_URL: 'server-public-api',
+};
+
+const signupUser = async (userData) => {
+    try {
+        const response = await axios.post(
+            `${Config.API_URL}/auth/signup`,
+            userData
+        );
+        return response;
+    } catch (error) {
+        console.error(
+            'Error during signup:',
+            error.response?.data || error.message
+        );
+        throw error;
+    }
+};
+
+const loginUser = async (userData) => {
+    try {
+        const response = await axios.post(
+            `${Config.API_URL}/auth/login`,
+            userData
+        );
+        return response;
+    } catch (error) {
+        console.error(
+            'Error during login:',
+            error.response?.data || error.message
+        );
+        throw error;
+    }
+};
+
+// Logout a user by sending their refresh token to the API
+const logoutUser = async (refreshToken: string) => {
+    try {
+        const response = await axios.post(`${Config.API_URL}/auth/logout`, {
+            token: refreshToken,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(
+            'Error during logout:',
+            error.response?.data || error.message
+        );
+        throw new Error('Logout failed. Please try again.');
+    }
+};
+
+const refreshAccessToken = async (refreshToken) => {
+    try {
+        const response = await fetch(`${Config.API_URL}/token`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ token: refreshToken }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data.accessToken;
+        }
+        throw new Error('Failed to refresh access token');
+    } catch (e) {
+        console.error('Error refreshing token:', e);
+        return null;
+    }
+};
+
+export { signupUser, loginUser, logoutUser, refreshAccessToken };
