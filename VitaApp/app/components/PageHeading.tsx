da1d3db69ce2692eb9620709443612c4797b5d1d
Dhanvin Patel <78669309+dhanvinpatel@users.noreply.github.com>
2024-11-14 18:02:17 -0500

    Added page heading component

---
+++ VitaApp/app/components/PageHeading.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Colors } from '../../constants/Colors';
+import { useRouter } from 'expo-router';
+
+const PageHeading = ({ title, handlePress }) => {
+    const router = useRouter();
+    return (
+        <View style={styles.header}>
+            <TouchableOpacity
+                onPress={handlePress}
+                style={styles.backButton}>
+                <Ionicons name='arrow-back' size={30} color={Colors.blue.primary} />
+            </TouchableOpacity>
+            <Text style={styles.headerTitle}>{title}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: '5%',
+        paddingVertical: "8%",
+    },
+    headerTitle: {
+        flex: 1,
+        fontSize: 32,
+        fontWeight: '600',
+        textAlign: 'center',
+        color: Colors.blue.primary,
+        paddingRight: '10%',
+    },
+    backButton: {
+        backgroundColor: Colors.blue.light,
+        borderRadius: 50,
+        padding: '2%',
+    },
+});
+
+export default PageHeading;
